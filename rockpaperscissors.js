26edc57c30fbbc0baca207ed8b21f425885edb99
Gabriel Poulard <gipoulard@gmail.com>
2024-06-10 22:59:08 -0400

    game is now played with buttons on the webpage

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -17,31 +17,30 @@ function getComputerChoice () {
     }
 }
 
-function getHumanChoice (num) {
-    if (num === 1) {
-        let choice = "Rock";
-        return choice;
-    } else if (num === 2) {
-        let choice = "Paper";
-        return choice;
-    } else if (num === 3) {
-        let choice = "Scissors";
-        return choice;
-    }
-}
+// function getHumanChoice (num) {
+//     if (num === 1) {
+//         let choice = "Rock";
+//         return choice;
+//     } else if (num === 2) {
+//         let choice = "Paper";
+//         return choice;
+//     } else if (num === 3) {
+//         let choice = "Scissors";
+//         return choice;
+//     }
+// }
 
 
-// Logic for the Game
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
 
 // This is the function to play a rock paper scissors round
-function playRound(humanChoice, computerChoice) {
+function playRound(humanChoice) {
 // This is to check if there is a draw by seeing if they have the same move chosen. 
+    const computerChoice = getComputerChoice();
+    let resultMessage = `Human player chose ${humanChoice} and computer player chose ${computerChoice}.`
+
     if (humanChoice === computerChoice) {
-        console.log(`Human player chose ${humanChoice} and computer player chose ${computerChoice}`)
-        console.log("It's a draw");
+        resultMessage += "It's a draw.";
+        updateResult(resultMessage);
         return "draw";
     }
 // Short circuiting lets us check for human victory condition.
@@ -49,39 +48,52 @@ function playRound(humanChoice, computerChoice) {
     if ((humanChoice === "Rock" && computerChoice === "Scissors") 
     || (humanChoice === "Scissors" && computerChoice === "Paper")
     || (humanChoice === "Paper" && computerChoice === "Rock")) {
-        console.log(`Human player chose ${humanChoice} and computer player chose ${computerChoice}`)
-        console.log("Congratulations, you have defeated the computer. +1 to your point total.");
+        resultMessage += " Congratulations, you have defeated the computer. +1 to your point total.";
+        updateResult(resultMessage);
         return "humanWon";
     } else {
-        console.log(`Human player chose ${humanChoice} and computer player chose ${computerChoice}`)
-        console.log("You have lost this round. +1 to the computer's point total");
+        resultMessage += " You have lost this around. +1 to the computer's point total.";
+        updateResult(resultMessage);
         return "computerWon";
     }
 }
 
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
+// function playGame() {
+//     let humanScore = 0;
+//     let computerScore = 0;
 
-    while (true) {
-        if(humanScore === 5 || computerScore === 5) {
-            console.log("The game is over.")
-            break;
-        }
-        let status = playRound(getHumanChoice(1), getComputerChoice());
+//     while (true) {
+//         if(humanScore === 5 || computerScore === 5) {
+//             console.log("The game is over.")
+//             break;
+//         }
+//         let status = playRound(getHumanChoice(1), getComputerChoice());
 
-        if(status === "humanWon") {
-            humanScore += 1;
-            console.log("Human's new score is: " + humanScore);
-        }
+//         if(status === "humanWon") {
+//             humanScore += 1;
+//             console.log("Human's new score is: " + humanScore);
+//         }
 
-        if(status === "computerWon") {
-            computerScore += 1;
-            console.log("Computer's new score is: " + computerScore);
-        }
-    }
+//         if(status === "computerWon") {
+//             computerScore += 1;
+//             console.log("Computer's new score is: " + computerScore);
+//         }
+//     }
+// }
+
+
+// playGame();
+
+
+// Logic to control the buttons
+
+function updateResult(message) {
+    const resultDiv = document.getElementById("results");
+    resultDiv.textContent = message;
 }
 
+document.querySelector("#rockbtn").addEventListener("click", () => playRound("Rock"));
+document.querySelector("#paperbtn").addEventListener("click", () => playRound("Paper"));
+document.querySelector("#scissorsbtn").addEventListener("click", () => playRound("Scissors"));
 
-playGame();
